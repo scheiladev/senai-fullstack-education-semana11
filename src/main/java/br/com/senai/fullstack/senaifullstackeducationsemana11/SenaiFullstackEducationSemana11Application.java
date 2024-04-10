a3308bf882da77d9104bb82506068b7e8d27006b
Scheila Stihler <sstihler@gmail.com>
2024-04-10 16:58:41 -0300

    Initial commit

---
+++ src/main/java/br/com/senai/fullstack/senaifullstackeducationsemana11/SenaiFullstackEducationSemana11Application.java
@@ -0,0 +1,13 @@
+package br.com.senai.fullstack.senaifullstackeducationsemana11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenaiFullstackEducationSemana11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenaiFullstackEducationSemana11Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/senai/fullstack/senaifullstackeducationsemana11/SenaiFullstackEducationSemana11ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senai.fullstack.senaifullstackeducationsemana11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenaiFullstackEducationSemana11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
